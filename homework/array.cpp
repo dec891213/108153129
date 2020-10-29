f4fecca8c79ee1c7c3faea01bcf1004b24ddc0ef
dec891213 <72922284+dec891213@users.noreply.github.com>
2020-10-29 22:31:17 +0800

    Update array.cpp

--- homework/array.cpp
+++ homework/array.cpp
@@ -4,27 +4,32 @@ using namespace std;
 
 int a[3][3] = {{1, 2, 3},{4, 5, 6},{7, 8, 9}};
 
-int get(int i, int j){
+int get(int i, int j)
+{
     i--;
     j--;
     return (a+(i*3)+j);
 }
-void set(int i, int j, int v){
+void set(int i, int j, int v)
+{
     i--;
     j--;
     int h = &(a+(i3)+j);
     *h = v;
 }
-void dump(){
+void dump()
+{
         cout << "已知二維陣列 轉 一維陣列";
-        for(int i=0; i<sizeof(a)/sizeof(int); i++){
+        for(int i=0; i<sizeof(a)/sizeof(int); i++)
+        {
             cout << (a+i) << " ";
             cout << *(a+i) << " ";
         }
         cout << endl;
     }
 
-int main(){
+int main()
+{
     dump();
     int x = 1, y = 2;
     cout << "get(2, 2) 第2列 第2行 = " << get(2, 2) << endl;
